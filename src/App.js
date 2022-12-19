5dcf8bf35fef4efeeba32958da0f95490de968ea
jaroslavasd <jaroslavasd@wix.com>
2022-12-19 23:58:05 +0200

    add netlify.toml

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import './App.css'
 import { useEffect } from 'react'
-// import Header from './components/Header/Header'
+import Header from './components/Header/Header'
 import { useTelegram } from './hooks/useTelegram'
 import { Route, Routes } from 'react-router-dom'
 import ProductList from "./components/ProductList/ProductList"
 import Form from "./components/Form/Form"
 
 function App()  {
-  const { tg, onToggleButton } = useTelegram()
+  const { tg } = useTelegram()
 
   useEffect(() => tg.ready(), [])
 
   return (
     <div className="App">
-      {/* <Header />
-      <button onClick={onToggleButton}>toggle</button> */}
+      <Header />
       <Routes>
-        <Route index element={<ProductList />}/>
-        <Route path={'products'} element={<ProductList />}/>
-        <Route path={'form'} element={<Form />}/>
+          <Route index element={<ProductList />}/>
+          <Route path={'form'} element={<Form />}/>
       </Routes>
     </div>
   );
